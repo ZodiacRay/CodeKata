5427c4b7597b5dfb2cc5469bacd90b0528a462e9
minsung <99423006+zarame3636@users.noreply.github.com>
2025-12-22 10:12:16 +0900

    2~5번 문제 해결

--- Codekata/Codekata/December2025.cpp
+++ Codekata/Codekata/December2025.cpp
@@ -1,10 +1,45 @@
 #include "December2025.h"
 
+// ==============
+// 2025-12-22
+// ==============
 
-// 2025-12-22 두수의 차 구하기 
-//https://school.programmers.co.kr/learn/courses/30/lessons/120803
-int Solution22(int num1, int num2)
+// 두수의 차 구하기 
+// https://school.programmers.co.kr/learn/courses/30/lessons/120803
+
+int Solution001(int num1, int num2)
 {
 	// 두수의 차를 리턴한다. 
 	return num1 - num2;
+}
+
+// 두수의 곱 구하기 
+// https://school.programmers.co.kr/learn/courses/30/lessons/120804
+
+int Solution002(int num1, int num2)
+{
+	return num1 * num2;
+}
+
+
+// 몫 구하기 
+// https://school.programmers.co.kr/learn/courses/30/lessons/120805
+
+int Solution003(int num1, int num2)
+{
+	return num1 / num2;
+}
+
+// 나이 출력
+// https://school.programmers.co.kr/learn/courses/30/lessons/120820
+int Solution004(int age)
+{
+	return 2022 - age + 1;
+}
+
+// 숫자 비교하기 
+// https://school.programmers.co.kr/learn/courses/30/lessons/120807
+int Solution005(int num1, int num2)
+{
+	return num1 == num2 ? 1 : -1;
 }
--- Codekata/Codekata/December2025.h
+++ Codekata/Codekata/December2025.h
@@ -1,4 +1,16 @@
 #pragma once
 
-// 2025-12-22 두수의 차 구하기 
-int Solution22(int num1, int num2); 
+
+// ================
+// 2025-12-22 
+// ================
+
+int Solution001(int num1, int num2); 
+
+int Solution002(int num1, int num2);
+
+int Solution003(int num1, int num2); 
+
+int Solution004(int age);
+
+int Solution005(int num1, int num2);
