65bcddd845c106ae51276d78962c2ba256f3a4b4
minsung <99423006+zarame3636@users.noreply.github.com>
2025-12-23 09:36:45 +0900

    Solve 002 ~ 014

--- Codekata/Codekata/December2025.cpp
+++ Codekata/Codekata/December2025.cpp
@@ -13,9 +13,13 @@ int Solution001(int num1, int num2)
 	return num1 - num2;
 }
 
+
+// ==============
+// 2025-12-23
+// ==============
+
 // 두수의 곱 구하기 
 // https://school.programmers.co.kr/learn/courses/30/lessons/120804
-
 int Solution002(int num1, int num2)
 {
 	return num1 * num2;
@@ -43,3 +47,101 @@ int Solution005(int num1, int num2)
 {
 	return num1 == num2 ? 1 : -1;
 }
+
+// 두 수의 합
+// https://school.programmers.co.kr/learn/courses/30/lessons/120802
+int Solution006(int num1, int num2)
+{
+	return num1 + num2; 
+}
+
+// 두 수의 나눗셈
+// https://school.programmers.co.kr/learn/courses/30/lessons/120806
+int Solution007(int num1, int num2)
+{
+	return (num1 * 1000 / num2);
+}
+
+// 각도기 
+// https://school.programmers.co.kr/learn/courses/30/lessons/120829
+int Solution008(int angle)
+{
+	if (angle > 0 && angle < 90) return 1;
+
+	if (angle == 90) return 2;
+
+	if (angle < 180) return 3;
+
+	if (angle == 180) return 4;
+
+	return -1;
+}
+
+// 짝수의 합
+// https://school.programmers.co.kr/learn/courses/30/lessons/120831
+int Solution009(int n)
+{
+	int ret = 0;
+
+	for (int i = 0; i <= n; i += 2)
+		ret += i;
+
+	return ret; 
+}
+
+// 배열의 평균 값
+// https://school.programmers.co.kr/learn/courses/30/lessons/12937
+double Solution010(std::vector<int> numbers)
+{
+	int sum = 0;
+
+	for (int num : numbers) sum += num;
+
+	return (double)(sum) / numbers.size();
+}
+
+// 짝수와 홀수
+// https://school.programmers.co.kr/learn/courses/30/lessons/12937
+std::string Solution011(int num)
+{
+	return num % 2 ? "Odd" : "Even";
+}
+
+// 평균 구하기 
+// https://school.programmers.co.kr/learn/courses/30/lessons/12944
+double Solution012(std::vector<int> arr)
+{
+	int sum = 0;
+
+	for (int n : arr) sum += n;
+
+	return (double)sum / arr.size(); 
+}
+
+// 자릿수 더하기 
+// https://school.programmers.co.kr/learn/courses/30/lessons/12931
+int Solution013(int n)
+{
+	int ret = 0;
+	while (n > 0)
+	{
+		ret += n % 10;
+		n /= 10;
+	}
+
+	return ret; 
+}
+
+// 약수의 합
+// https://school.programmers.co.kr/learn/courses/30/lessons/12928
+int Solution014(int n)
+{
+	int ret = 0;
+	for (int i = 0; i <= n; i++)
+	{
+		if (n % i == 0) ret += i; 
+	}
+
+	return ret ; 
+}
+
--- Codekata/Codekata/December2025.h
+++ Codekata/Codekata/December2025.h
@@ -1,11 +1,17 @@
 #pragma once
-
+#include <vector>
+#include <string>
 
 // ================
 // 2025-12-22 
 // ================
 
 int Solution001(int num1, int num2); 
+
+
+// ================
+// 2025-12-23 
+// ================
 
 int Solution002(int num1, int num2);
 
@@ -14,3 +20,21 @@ int Solution003(int num1, int num2);
 int Solution004(int age);
 
 int Solution005(int num1, int num2);
+
+int Solution006(int num1, int num2);
+
+int Solution007(int num1, int num2);
+
+int Solution008(int angle);
+
+int Solution009(int n);
+
+double Solution010(std::vector<int> numbers); 
+
+std::string Solution011(int num);
+
+double Solution012(std::vector<int> arr);
+
+int Solution013(int n);
+
+int Solution014(int n); 
